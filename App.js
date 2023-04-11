bb539f2c44b86c7010f06ec1f8d2536b62a963c2
Stefano Cutri <stefanocutristecco@gmail.com>
2023-04-11 18:25:46 -0300

    Move to FlatList

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, FlatList, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
 
@@ -13,24 +13,45 @@ export default function App() {
 
   const addGoal = () => {
     if (text.length > 0) {
-      setGoals(prev => [...prev, text])    
+      setGoals(prev => [...prev, 
+        {text: text, key: Math.random().toString()}
+      ])    
     }
+  }
+
+  const GoalItem = ({item}) => {
+  return (
+    <View style={styles.goalItem}>
+    <Text style={styles.goalText}>{item}</Text>
+  </View>
+  )
+  
   }
 
   return (
     <View style={styles.container}>
       <View style={styles.inpuContainer}>
-        <TextInput placeholder='Enter your goals for this year!' onChangeText={handleTextChange}/>
+        <TextInput placeholder='Enter your goals for this year!' style={styles.textInput} onChangeText={handleTextChange}/>
         <Button title='Add Goal' onPress={addGoal}/>
       </View>
-      <View style={styles.goalsContainer}>
-      <Text>List of goals...</Text>
-        {
-          goals.map((goal, i) => (
-            <Text key={i}>{goal}</Text>
-          ))
-        }
-      </View>
+
+      {
+        goals.length > 0
+        ?
+        <View style={styles.goalsContainer}>
+        <FlatList 
+        data={goals}
+        renderItem={ ({item}) => (<GoalItem item={item.text}/>)}
+        keyExtractor={(item) => item.id}
+        />
+       
+        </View>
+        :
+        <View style={styles.goalsContainer}>
+          <Text>Start your journey!</Text>
+        </View>
+      }
+
     </View>
   );
 }
@@ -38,18 +59,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 16
   },
   inpuContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
     flex: 1,
-    width: '80%',
-    justifyContent: 'space-between'
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc'
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 8,
+    padding: 8
   },
   goalsContainer: {
     flex: 5
+  },
+  goalItem: {
+    padding: 8,
+    margin: 6,
+    backgroundColor: 'black',
+    borderRadius: 6
+  },
+  goalText: {
+    color: 'white'
   }
 });
